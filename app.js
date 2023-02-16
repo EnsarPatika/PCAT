6a5270c8ad59a0c1bedb808013c1683f1b74acc2
Ensar Uysal <eensaruysal@gmail.com>
2023-02-17 01:17:58 +0300

    Ejs edited

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs'); // For EJS
+
+app.use(express.static('public')); // For Static folder in EXPRESS
 
 const mylogger = (req, res, next) => {
+  // Mıddleware function
   console.log('Mylogger');
   next();
 };
 
-app.use(mylogger);
+// app.use(mylogger);
 
 app.get('/', (req, res, next) => {
-  let photo = {
-    id: 1,
-    description: 'Profile photo',
-    isAded: true,
-  };
-  res.send(photo);
+  res.render('index');
+});
+app.get('/about', (req, res, next) => {
+  res.render('about');
+});
+app.get('/addphoto', (req, res, next) => {
+  res.render('addphoto');
 });
 
 const port = 3000;
