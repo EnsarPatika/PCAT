10a1b3973815048a4bbcf1915831f9907e00dcd3
Ensar Uysal <eensaruysal@gmail.com>
2023-02-21 00:52:22 +0300

    Image upload issue completed

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
 const app = express();
 const Photo = require('./models/Photo.js');
+const fileUpload = require('express-fileupload');
+
+// EJS
 app.set('view engine', 'ejs'); // For EJS
 
+// DATABASE
 mongoose.connect('mongodb://127.0.0.1:27017/PCAT');
 
+// MIDDLEWARE
 app.use(express.static('public')); // For Static folder in EXPRESS
 app.use(express.urlencoded({ extended: true })); // For Post request encoding in EXPRESS
 app.use(express.json()); // For Post request json formated in EXPRESS
+app.use(fileUpload());
+
+//ROUTES
 const mylogger = (req, res, next) => {
   // Mıddleware function
   console.log('Mylogger');
@@ -32,8 +41,24 @@ app.get('/addphoto', (req, res, next) => {
   res.render('addphoto');
 });
 app.post('/photos', async (req, res, next) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  // console.log(req.files.image);
+  const uploadway = 'public/upload';
+
+  if (!fs.existsSync(uploadway)) {
+    fs.mkdirSync(uploadway);
+  }
+  let imagevar = req.files.image;
+  let imageName = req.files.image.name;
+  let uploadPath = __dirname + '/'+ uploadway + '/' + imageName;
+  imagevar.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/upload/'+ imageName ,
+    });
+    res.redirect('/');
+  });
+
+  // await Photo.create(req.body);
 });
 app.get('/photopage/:id', async (req, res, next) => {
   const photo = await Photo.find({ _id: req.params.id });
